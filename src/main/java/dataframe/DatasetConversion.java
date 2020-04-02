964d4a607955c926e1e8653c620468bd6c8cac06
devd1994 <devkg94@gmail.com>
2020-04-02 18:15:47 +0530

    Revert "added id2 & id3"
    This reverts commit 297b9726

--- src/main/java/dataframe/DatasetConversion.java
+++ src/main/java/dataframe/DatasetConversion.java
@@ -21,10 +21,6 @@ public class DatasetConversion {
         private int id;
         private int id1;
         private int id2;
-
-        private int id3;
-        private int id4;
-
         private String name;
         private double sales;
         private double discount;
